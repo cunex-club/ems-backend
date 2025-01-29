7718d03ac9874964e0e25a2dc4af7d0e767c6608
Jimmy-Tempest <smartwattana@gmail.com>
2025-01-29 15:24:52 +0700

    setup: reduced mysk-api lib

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,4 +1,2 @@
 pub(crate) mod common;
-pub(crate) mod error;
 pub(crate) mod models;
-pub(crate) mod prelude;
--- src/lib/prelude.rs
+++
@@ -1,3 +0,0 @@
-pub use crate::lib::error::Error;
-
-pub type Result<T, E = Error> = core::result::Result<T, E>;
