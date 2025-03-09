edaaf555885c46d3383a6fb36a85dfc1175dea51
Jimmy-Tempest <smartwattana@gmail.com>
2025-03-09 17:42:32 +0700

    feat: delete question route

---
+++ src/routes/v1/questions/delete_question.rs
@@ -0,0 +1,54 @@
+use crate::{
+    extractors::{api_key::ApiKeyHeader, logged_in::LoggedIn},
+    models::{
+        question::{db::DbQuestion, Question},
+        Authorize,
+    },
+    AppState,
+};
+use actix_web::{
+    delete,
+    web::{Data, Path},
+    HttpResponse, Responder,
+};
+use mysk_lib::{
+    common::{
+        requests::{RequestType, SortablePlaceholder},
+        response::ResponseType,
+    },
+    models::traits::GetById,
+    permissions::ActionType,
+    prelude::*,
+    query::QueryablePlaceholder,
+};
+use sqlx::query;
+use uuid::Uuid;
+
+#[delete("/{id}")]
+pub async fn delete_question(
+    data: Data<AppState>,
+    _: ApiKeyHeader,
+    LoggedIn(user): LoggedIn,
+    question_id: Path<Uuid>,
+    _: RequestType<(), QueryablePlaceholder, SortablePlaceholder>,
+) -> Result<impl Responder> {
+    let pool = &data.db;
+    let question_id = question_id.into_inner();
+
+    // Check if election exists
+    let question = DbQuestion::get_by_id(pool, question_id).await?;
+
+    // Check if user has permission to delete election
+    question
+        .authorize(user.id, pool, ActionType::Delete)
+        .await?;
+
+    // Delete election
+    query!("DELETE FROM questions WHERE id = $1", question_id)
+        .execute(pool)
+        .await?;
+
+    let response: ResponseType<Option<Question>> = ResponseType::new(None, None);
+
+    Ok(HttpResponse::Accepted().json(response))
+}
--- src/routes/v1/questions/mod.rs
+++ src/routes/v1/questions/mod.rs
@@ -1,6 +1,7 @@
 use actix_web::web::ServiceConfig;
 
 pub mod create_question;
+pub mod delete_question;
 pub mod get_question_details;
 pub mod modify_question;
 pub mod query_question;
@@ -10,4 +11,5 @@ pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(create_question::create_question);
     cfg.service(query_question::query_question);
     cfg.service(modify_question::modify_question);
+    cfg.service(delete_question::delete_question);
 }
